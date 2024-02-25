7cbd4b32ca0ea74681018de28157bbbc359cc358
Cosmin Curuliuc <cosmincuruliuc@gmail.com>
2024-02-25 18:15:35 +0200

    Versiune initiala

---
+++ SD_Laborator_1/src/main/java/ReadStudentServlet.java
@@ -0,0 +1,30 @@
+import beans.StudentBean;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+
+public class ReadStudentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException{ // deserializare student din fisierul XML de pe disc
+        File file = new File("/home/student/IdeaProjects/SD_Laborator_1/student.xml");
+        // se returneaza un raspuns HTTP de tip 404 in cazul incarenu se gaseste fisierul cu date
+        if (!file.exists()) {
+            response.sendError(404, "Nu a fost gasit niciun studentserializat pe disc!");
+            return;
+        }
+        XmlMapper xmlMapper = new XmlMapper();
+        StudentBean bean = xmlMapper.readValue(file,
+                StudentBean.class);
+        request.setAttribute("nume", bean.getNume());
+        request.setAttribute("prenume", bean.getPrenume());
+        request.setAttribute("varsta", bean.getVarsta());
+
+        // redirectionare date catre pagina de afisare a informatiilor studentului
+        request.getRequestDispatcher("./info-student.jsp").forward(request, response);
+    }
+}
