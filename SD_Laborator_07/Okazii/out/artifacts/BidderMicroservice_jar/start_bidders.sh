23556af2c4fdc74439a58eeaa47baa6946a5d1d4
Cosmin Curuliuc <cosmincuruliuc@gmail.com>
2024-04-07 00:40:43 +0300

    Laborator 7 - Script rulare 100 instante Bidder

---
+++ SD_Laborator_07/Okazii/out/artifacts/BidderMicroservice_jar/start_bidders.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+
+# Numărul de instanțe Bidder pe care vrei să le lansezi
+NUM_INSTANCES=100
+
+# Bucla care lansează instanțele
+for (( i=1; i<=NUM_INSTANCES; i++ ))
+do
+   echo "Starting Bidder instance $i"
+   java -jar BidderMicroservice.jar &
+done
+
+echo "$NUM_INSTANCES instances of Bidder Microservice have been started."
